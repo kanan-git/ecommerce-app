3c20c6d550d2319aa4dbc71e287b582eff371877
kanan-git <kb.coders@mail.ru>
2023-07-14 15:14:09 +0400

    work in progress (html & scss) - settings, login, recovery, supportMore, blog, blogArticle, myCart, myOrders, myFav

--- src/component and pages/Settings/Settings.jsx
+++ src/component and pages/Settings/Settings.jsx
@@ -75,87 +75,119 @@ function Settings() {
                         </span>
 
                         <span className="settings__container--group_parameters" id="0" style={{display: "flex"}}>
-                            "profile", // 0
-                            <br />
-                            "profile", // 0
-                            <br />
-                            "profile", // 0
-                            <br />
-                            "profile", // 0
-                            <br />
-                            "profile", // 0
-                            <br />
-                            "profile", // 0
+                            <div className="upside">
+                                <h3 className="upside__title"> {textDataBaseSTATE.buttons[0]} </h3>
+                                <p className="upside__userid"> User ID: 0 </p>
+                            </div>
+
+                            <div className="container">
+                                <div className="container__profile">
+                                    <img src="/asset_library/user_profile_images/profile_guest.png" 
+                                    alt="user-image" className="container__profile--image" />
+                                    <span className="container__profile--group">
+                                        <button className="container__profile--group_buttons">
+                                            <img src="/asset_library/symbols_svg/edit-3-svgrepo-com.svg" 
+                                            className="container__profile--group_buttons-symbol" />
+                                            edit
+                                        </button>
+                                        <button className="container__profile--group_buttons">
+                                            <img src="/asset_library/symbols_svg/xmark-circle-svgrepo-com.svg" 
+                                            className="container__profile--group_buttons-symbol" />
+                                            remove
+                                        </button>
+                                    </span>
+                                </div>
+                                <div className="container__username">
+                                    <span className="container__username--name">
+                                        name:
+                                        <input type="text" className="container__username--name_input" 
+                                        placeholder="nameOfTheUser" />
+                                    </span>
+                                    <span className="container__username--name">
+                                        lastname:
+                                        <input type="text" className="container__username--name_input" 
+                                        placeholder="lastnameOfTheUser" />
+                                    </span>
+                                </div>
+                                <div className="container__gender">
+                                    gender:
+                                    <select name="gender" id="gender" className="container__gender--input">
+                                        <option value="Male" className="container__gender--input_options">
+                                            Male
+                                        </option>
+                                        <option value="Female" className="container__gender--input_options">
+                                            Female
+                                        </option>
+                                    </select>
+                                </div>
+                                <div className="container__dateofbirth">
+                                    date of birth:
+                                    <select name="month" id="month" className="container__dateofbirth--input">
+                                        <option value="January" className="container__dateofbirth--input_options">
+                                            January
+                                        </option>
+                                        <option value="December" className="container__dateofbirth--input_options">
+                                            December
+                                        </option>
+                                    </select>
+                                </div>
+                                <button className="container__submit"> save changes </button>
+                            </div>
                         </span>
 
                         <span className="settings__container--group_parameters" id="1" style={{display: "none"}}>
-                            "password & security", // 1
-                            <br />
-                            "password & security", // 1
-                            <br />
-                            "password & security", // 1
-                            <br />
-                            "password & security", // 1
-                            <br />
-                            "password & security", // 1
-                            <br />
-                            "password & security", // 1
+                            <div className="upside">
+                                <h3 className="upside__title"> {textDataBaseSTATE.buttons[1]} </h3>
+                                <p className="upside__userid"> User ID: 0 </p>
+                            </div>
+
+                            <div className="container">
+                                .
+                            </div>
                         </span>
 
                         <span className="settings__container--group_parameters" id="2" style={{display: "none"}}>
-                            "email preferences", // 2
-                            <br />
-                            "email preferences", // 2
-                            <br />
-                            "email preferences", // 2
-                            <br />
-                            "email preferences", // 2
-                            <br />
-                            "email preferences", // 2
-                            <br />
-                            "email preferences", // 2
+                            <div className="upside">
+                                <h3 className="upside__title"> {textDataBaseSTATE.buttons[2]} </h3>
+                                <p className="upside__userid"> User ID: 0 </p>
+                            </div>
+
+                            <div className="container">
+                                .
+                            </div>
                         </span>
 
                         <span className="settings__container--group_parameters" id="3" style={{display: "none"}}>
-                            "notifications", // 3
-                            <br />
-                            "notifications", // 3
-                            <br />
-                            "notifications", // 3
-                            <br />
-                            "notifications", // 3
-                            <br />
-                            "notifications", // 3
-                            <br />
-                            "notifications", // 3
+                            <div className="upside">
+                                <h3 className="upside__title"> {textDataBaseSTATE.buttons[3]} </h3>
+                                <p className="upside__userid"> User ID: 0 </p>
+                            </div>
+
+                            <div className="container">
+                                .
+                            </div>
                         </span>
 
                         <span className="settings__container--group_parameters" id="4" style={{display: "none"}}>
-                            "payment methods", // 4
-                            <br />
-                            "payment methods", // 4
-                            <br />
-                            "payment methods", // 4
-                            <br />
-                            "payment methods", // 4
-                            <br />
-                            "payment methods", // 4
-                            <br />
-                            "payment methods", // 4
+                            <div className="upside">
+                                <h3 className="upside__title"> {textDataBaseSTATE.buttons[4]} </h3>
+                                <p className="upside__userid"> User ID: 0 </p>
+                            </div>
+
+                            <div className="container">
+                                .
+                            </div>
                         </span>
 
                         <span className="settings__container--group_parameters" id="5" style={{display: "none"}}>
-                            "delete account" // 5
-                            <br />
-                            "delete account" // 5
-                            <br />
-                            "delete account" // 5
-                            <br />
-                            "delete account" // 5
-                            <br />
-                            "delete account" // 5
-                            <br />
-                            "delete account" // 5
+                            <div className="upside">
+                                <h3 className="upside__title"> {textDataBaseSTATE.buttons[5]} </h3>
+                                <p className="upside__userid"> User ID: 0 </p>
+                            </div>
+
+                            <div className="container">
+                                .
+                            </div>
                         </span>
 
                     </div>
